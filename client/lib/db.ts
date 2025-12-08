b3367bd4f2d23f308ae8155da6c06a62a2cb8010
Emmanuel Leo Demby <emmanuelleosamuel203@gmail.com>
2025-12-07 20:18:11 -0500

    update2.0

--- client/lib/db.ts
+++ client/lib/db.ts
@@ -8,6 +8,7 @@ declare global {
 export const db =
   (globalThis as any).prisma ??
   new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL,
     log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   })
 
